c59d19728de9161b91070f9712b0b880762731ab
dyegotech <dsoliveira.tech@gmail.com>
2024-09-23 19:01:26 -0500

    Classes Mentoria e Curso criadas

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso c1 = new Curso();
+
+        c1.setTitulo("Java Básico");
+        c1.setDescricao("Java Backend para iniciantes");
+        c1.setCargaHoraria(200);
+
+        Mentoria m1 = new Mentoria();
+        m1.setTitulo("Mentoria em Java");
+        m1.setDescricao("Java para inciantes");
+        m1.setData(LocalDate.now());
+
+        System.out.println(c1);
+        System.out.println(m1);
+
+    }
+}
